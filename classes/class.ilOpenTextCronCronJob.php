4b2ff04a5a5b917bd7f1386da68b095289c3ee57
smeyer <smeyer.ilias@gmx.de>
2019-07-09 17:11:29 +0200

    Added opentext cron job wrapper

---
+++ classes/class.ilOpenTextCronCronJob.php
@@ -0,0 +1,104 @@
+<?php
+/* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+
+/**
+ * Open Text Cron job definition
+ * 
+ * @author Stefan Meyer <smeyer.ilias@gmx.de>
+ *
+ */
+class ilOpenTextCronCronJob extends ilCronJob
+{
+	const DEFAULT_SCHEDULE_TIME = 1;
+
+	/**
+	 * @var \ilOpenTextCronPlugin
+	 */
+	protected $plugin;
+
+	/**
+	 * @return string
+	 */
+	public function getId()
+	{
+		return ilOpenTextCronPlugin::getInstance()->getId();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return ilOpenTextCronPlugin::PNAME;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return \ilOpenTextCronPlugin::getInstance()->txt('cron_job_info');
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getDefaultScheduleType()
+	{
+		return self::SCHEDULE_TYPE_IN_HOURS;
+	}
+
+	/**
+	 * @return array|int
+	 */
+	public function getDefaultScheduleValue()
+	{
+		return self::DEFAULT_SCHEDULE_TIME;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasAutoActivation()
+	{
+		return false;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasFlexibleSchedule()
+	{
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasCustomSettings() 
+	{
+		return false;
+	}
+
+	/**
+	 * @return \ilCronJobResult
+	 */
+	public function run()
+	{
+		$result = new ilCronJobResult();
+		$result->setStatus(ilCronJobResult::STATUS_OK);
+		return $result;
+	}
+
+	/**
+	 * @return \ilOpenTextCronCronJob
+	 */
+	public function getPlugin()
+	{
+		return \ilOpenTextCronPlugin::getInstance();
+	}
+
+}
+
+?>
