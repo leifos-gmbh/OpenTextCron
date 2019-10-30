b28370a14330cc8ecb8de90b0db41c7787054d65
smeyer <smeyer.ilias@gmx.de>
2019-10-30 17:26:31 +0100

    Fixed plugin version

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = 'otxtc';
 
 // code version; must be changed for all code changes
-$version = '5.3.16.0';
+$version = '5.4.16.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.3.16";
-$ilias_max_version = "5.3.99";
+$ilias_min_version = "5.4.6";
+$ilias_max_version = "5.4.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
